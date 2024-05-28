df67c46cee347f50636734ee72740f946eb0242f
Linet <teweilinet@gmail.com>
2024-05-28 09:26:09 +0300

    many to many

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,36 +3,42 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+ #the use of ._ is to show that the attribute should not be accessed 
 class Author:
     def __init__(self, name):
         self.name = name
+        self._articles = [] #array of articles that author has written
 
     def articles(self):
-        pass
+        return self._articles #returns a list of articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self._articles)) # a unique list of magazines for which the author has contributed to
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title) #creation of an article instance
+        self._articles.append(article) #adds the magazine and title to the article list
+        return article
 
     def topic_areas(self):
-        pass
+        return list(set(article.magazine.category for article in self._articles)) #unique list of categories of magazines for which the author has written articles.
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+        self._contributors = set()
 
     def articles(self):
-        pass
+        return self._articles #a list of all the articles the magazine has published
 
     def contributors(self):
-        pass
+        return list(self._contributors) #a unique list of authors who have written for this magazine
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles] #returns a list of titles of all articles
 
     def contributing_authors(self):
-        pass
+        authors=[article.author for article in self._articles]
+        return [author for author in set(authors) if authors.counts() > 2]
